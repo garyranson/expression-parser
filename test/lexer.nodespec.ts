0618c63ff5f725c7f168b2b209963d5b14862ce8
garyranson <garyranson@hotmail.com>
2017-04-12 19:26:17 +0100

    Latest

--- test/lexer.nodespec.ts
+++ test/lexer.nodespec.ts
@@ -6,7 +6,7 @@ describe("Lexer Reader Tests 1..", () => {
     let lr = new LexerReader("");
     expect(lr.next()).to.deep.equal({type: "eof", value: "", start: 0, end: 0});
   });
-  it("Simple Test 2", () => {
+  it("Simple Test 2.", () => {
     let lr = new LexerReader("one");
     expect(lr.next()).to.deep.equal({type: "token", value: "one", start: 0, end: 2});
     expect(lr.next()).to.deep.equal({type: "eof", value: "", start: 3, end: 3});
